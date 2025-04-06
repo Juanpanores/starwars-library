8795bafda14aebd5d682f427173d50b5870cfd91
Juan Pablo Nores <noresjpn@gmail.com>
2025-04-06 00:10:59 -0300

    Implement filters and cache

--- src/components/CharacterTable.vue
+++ src/components/CharacterTable.vue
@@ -1,109 +1,201 @@
 <template>
   <div class="table-container">
     <h1 class="title">Star Wars Wiki</h1>
-    <input
-      type="text"
-      v-model="searchQuery"
-      @input="onSearchInput"
-      placeholder="Buscar personaje..."
-      class="search-input"
-    />
+    <div class="search-container">
+      <div class="search-input-container">
+        <p>Personajes</p>
+        <input
+          type="text"
+          v-model="searchQuery"
+          @input="onSearchInput"
+          placeholder="Buscar personaje..."
+          class="search-input"
+        />
+      </div>
+      <div class="dopdown-filter-container">
+        <div class="dropdown-filter">
+          <p>Vehiculo</p>
+          <select v-model="selectedVehicle" @change="onVehicleChange" class="dropdown">
+            <option value="">Todos</option>
+            <option v-for="vehicle in vehicles" :key="vehicle" :value="vehicle">
+              {{ vehicle }}
+            </option>
+          </select>
+        </div>
+        <div class="dropdown-filter">
+          <p>Especie</p>
+          <select v-model="selectedSpecies" @change="onSpeciesChange" class="dropdown">
+            <option value="">Todos</option>
+            <option v-for="species in species" :key="species" :value="species">
+              {{ species }}
+            </option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div v-if="characters.length === 0 && !skeletonLoading" class="no-results">
+      <p>Resultados, no hay.</p>
+    </div>
     <CharacterTableContent
+      v-else
       :characters="characters"
+      :skeletonLoading="skeletonLoading"
       :loading="loading"
-      :isFetchingMore="isFetchingMore"
     />
     <div class="fetch-more">
-      <button @click="fetchMore" :disabled="loading || isFetchingMore">
-        {{ loading || isFetchingMore ? 'Cargando...' : 'Cargar más' }}
+      <button @click="fetchMore" :disabled="skeletonLoading || loading">
+        {{ skeletonLoading || loading ? 'Cargando...' : 'Ver más' }}
       </button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import { useCharacters } from '@/composables/useCharacters';
 import CharacterTableContent from './CharacterTableContent.vue';
 
 const {
   characters,
+  skeletonLoading,
   loading,
-  isFetchingMore,
-  loadCharacters,
-  searchCharacters,
+  vehicles,
+  species,
+  selectedVehicle,
+  selectedSpecies,
+  searchQuery,
+  onSearchInput,
+  onVehicleChange,
+  onSpeciesChange,
   fetchMore,
 } = useCharacters();
-
-const searchQuery = ref('');
-let searchTimeout: number | undefined;
-
-function onSearchInput(event: Event) {
-  const value = (event.target as HTMLInputElement).value;
-  if (searchTimeout) clearTimeout(searchTimeout);
-
-  searchTimeout = window.setTimeout(() => {
-    searchCharacters(value);
-  }, 300);
-}
-
-loadCharacters();
 </script>
 
 <style scoped>
-.table-container {  
-  display: flex;
-  flex-direction: column;
-  justify-content: top;
-  align-items: center;
-  width: 100%;
-  padding: 1rem 10%;
-  background-color: rgba(0, 0, 0, 0.9);
-  border-radius: 2rem;
-}
+  .table-container {  
+    display: flex;
+    flex-direction: column;
+    justify-content: top;
+    align-items: center;
+    width: 100%;
+    padding: 1rem 10%;
+    background-color: rgba(0, 0, 0, 0.9);
+    border-radius: 2rem;
+  }
 
-.title {
-  font-size: 2.5rem;
-  color: yellow;
-  font-family: 'Star Jedi', sans-serif;
-  text-shadow: 2px 2px 4px gray;
-  margin-bottom: 1rem;
-  text-align: center;
-}
+  .title {
+    font-size: 2.5rem;
+    color: yellow;
+    font-family: 'Star Jedi', sans-serif;
+    text-shadow: 2px 2px 4px gray;
+    margin-bottom: 1rem;
+    text-align: center;
+  }
 
-.search-input {
-  margin-bottom: 1rem;
-  padding: 0.5rem;
-  width: 100%;
-  max-width: 400px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  font-size: 1rem;
-}
-table {
-  border-radius: 1rem;
-  overflow: hidden; 
-}
+  .search-container {
+    display: flex;
+    gap: 1rem;
+    margin-bottom: 1rem;
+    width: 100%;
+    justify-content: space-between;
+  }
 
-.fetch-more {
-  display: flex;
-  justify-content: center;
-  margin-top: 1rem;
-}
+  .search-input-container {
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+  }
 
-.fetch-more button {
-  padding: 0.5rem 1rem;
-  border: none;
-  background-color: #007bff;
-  color: white;
-  border-radius: 4px;
-  cursor: pointer;
-}
+  .dopdown-filter-container {
+    display: flex;
+    gap: 1rem;
+  }
 
-.fetch-more button:disabled {
-  background-color: #ccc;
-  cursor: not-allowed;
-}
+  .dropdown-filter {
+    width: 50%;
+  }
 
+  .search-input {
+    flex: 2;
+    padding: 0.5rem;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    font-size: 1rem;
+  }
 
+  .dropdown {
+    flex: 1;
+    width: 100%;
+    padding: 0.5rem;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    font-size: 1rem;
+    background-color: white;
+    min-width: 250px;
+  }
+
+  .no-results {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+    color: green;
+    width: 100%;
+    height: 200px;
+    border-radius: 1rem;
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  .fetch-more {
+    display: flex;
+    justify-content: center;
+    margin-top: 1rem;
+  }
+
+  .fetch-more button {
+    padding: 0.5rem 1rem;
+    border: none;
+    background-color: #007bff;
+    color: white;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .fetch-more button:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
+
+  @media (max-width: 1024px) {
+    .search-container {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      margin-bottom: 1rem;
+      width: 100%;
+      justify-content: space-between;
+    }
+
+    .dropdown {
+      flex: 1;
+      width: 100%;
+      padding: 0.5rem;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      font-size: 1rem;
+      background-color: white;
+    }
+  }
+
+  @media (max-width: 710px) {
+    .dopdown-filter-container {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+    }
+    .dropdown-filter {
+      width: 100%;
+    }
+  }
 </style>
